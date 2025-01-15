f6aa310aecf509a709ece2a1939d2b9869cf3cbd
the-coderRavi <ravigoku7061@gmail.com>
2025-01-15 11:55:43 +0530

    first commit

---
+++ chapter_three.py
@@ -0,0 +1,20 @@
+#type csating
+
+# a = "1" - this is a integers number
+# b= 2 this is a string number
+
+# so agar hame num ko add karana hai to to num string form me honea chahiye 
+
+a = "1"
+b = "2"
+print(a+b)
+# ye 1+2 = 12 ho jyega 
+a = 1
+b = 2
+print(a+b)
+# ye 1+2 = 3 hoaga 
+# ese codisons me hame string ko integer me convert krna hoga tabhi work karega 
+
+a = "1"
+b = "2"
+print(int(a) + int(b))
