da08f4350ba5b50700777c5f9a71833a41ae47e7
the-coderRavi <ravigoku7061@gmail.com>
2025-01-15 14:21:30 +0530

    chapter one practic

---
+++ practic_chapter_one.py
@@ -0,0 +1,19 @@
+# write a program to print a poem in python 
+print("twinkal twinkal little star, \nhow you wonder what your are,\n eating a sugar na na na, \n teling a lie ha ha ha| ")
+
+# use  vs coad and print a table of 5 
+print(5*1)
+print(5*2)
+print(5*3)
+print(5*4)
+print(5*5)
+print(5*6)
+print(5*7)
+print(5*8)
+print(5*9)
+print(5*10)
+
+# lable the program writte in problam 4 with comments
+# there are two type of comments the first is single line comment that the time we using hasstag 
+'''the second comment is three lline of comments that the we using three collom in comments
+ '''
