69a0a82f24222ec650a4a17cf92410715a04f652
the-coderRavi <ravigoku7061@gmail.com>
2025-01-16 14:14:02 +0530

    new things add

--- practic.py
+++ practic.py
@@ -1,1 +1,5 @@
 print("Hello i am Ravi prakash jaiswal and\n  i am in greduation and  i am done  my \"political science\" honures \n than i was learning about python languages ")
+
+print("hey", 6, 7, sep="~", end="009\n")
+
+print("Hello i am Ravi prakash jaiswal", 6, 7, sep="~", end="0099")
