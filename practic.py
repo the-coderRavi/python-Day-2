6b61fb0a32b35d46c546db1879cbbedf45f5ab3f
the-coderRavi <ravigoku7061@gmail.com>
2025-01-16 14:56:06 +0530

    calculator practic in python

--- practic.py
+++ practic.py
@@ -38,7 +38,7 @@ print(x ** b)
     # e = (1, 2, 3) = tuple
     # f = {1, 2, 3} = set
     # g = {1: "a", 2: "b", 3: "c"} = dictionary
-    
+
 print(" hey i am smart boy")
 
 a=1
@@ -50,3 +50,32 @@ print("the type of a is" ,type(a))
 print("the type of a is" ,type(b))
 print("the type of a is" ,type(c))
 print("the type of a is" ,type(d))
+
+print("calculator")
+
+print("1. Addition")
+
+print("2. Subtraction")
+
+print("3. Multiplication")
+
+print("4. Division")    
+
+choice = input("Enter your choice : ")
+
+if choice == "1":
+    num1 = float(input("Enter first number : "))
+    num2 = float(input("Enter second number : "))
+    print("Addition result : ", num1 + num2)
+if choice == "2":
+    num1 = float(input("enter your first number:"))
+    num2 = float(input("enter second number :"))
+    print("subtraction) result:", num1 - num2)
+if choice == "3":
+    num1 = float(input("enter your first number:"))
+    num2 = float(input("enter your second number:"))
+    print("multiplication result:", num1 * num2)
+if choice == "4":
+    num1 = float(input("enter your first number:" ))
+    num2= float(input("enter your second number:"))
+    print("devision result:", num1 / num2)
