eb802bf508485b62c45f702679941f9acce191ee
the-coderRavi <ravigoku7061@gmail.com>
2025-09-20 22:22:35 +0530

    new

--- chaptor_3.o.py
+++ chaptor_3.o.py
@@ -1,7 +1,9 @@
 # tankin user input in python
 a = input ("enter your name:")
-print("Enter your name:(a)")
-b = input ("enter first number:")
+print("my name is:" , a)
+
+
+b = input ("enter first number"))
 c = input ("enter second number:")
 print(b+c)
 
